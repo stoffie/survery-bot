d9ff7f9f00e26c80a626e37417bac0221a5cbf51
neboduus <neboduus@gmail.com>
2018-01-04 15:51:45 +0100

    adds dispatcher management in function of user state

--- lib/bot/classes/dispatcher.rb
+++ lib/bot/classes/dispatcher.rb
@@ -17,17 +17,46 @@ class Dispatcher
     else
       # dispatch in function of user state and text input
       aasm_state = @patient.aasm_state
-      ap "CURRENT USER: #{@patient.id} STATE: #{aasm_state}"
 
       case aasm_state
         when 'idle'
+          manage_idle(text)
 
+        when 'questionnaires'
+          manage_questionnaires(text)
 
-        else # 'feedbacking'
-
+        else # 'responding'
+          manage_responding(text)
 
       end
 
+    end
+  end
+
+  def manage_idle(text)
+    case text
+      when 'Ho da fare dei questionari?'
+        @patient.start_questionnaires!
+      else
+        @patient.no_action!
+    end
+  end
+
+  def manage_questionnaires(text)
+    case text
+      when *back_strings
+        @patient.cancel!
+      else
+        @patient.start_responding!(text)
+    end
+  end
+
+  def manage_responding(text)
+    case text
+      when *back_strings
+        @patient.cancel!
+      else
+        @patient.respond!(text)
     end
   end
 
@@ -36,7 +65,8 @@ class Dispatcher
   end
 
   def back_strings
-    ['Indietro', 'indietro', 'basta', 'Torna Indietro', 'Basta', 'back', 'Torna al Menu', 'Rispondi piu\' tardi/Torna al Menu']
+    ['Indietro', 'indietro', 'basta', 'Torna Indietro', 'Basta', 'back',
+     'Torna al Menu', 'Rispondi piu\' tardi/Torna al Menu', 'stop']
   end
 
   def tell_me_more_strings
