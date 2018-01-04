c11df653c0d9e843244904335c90f48294916436
neboduus <neboduus@gmail.com>
2018-01-04 15:49:53 +0100

    finilizes State Machine

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -24,18 +24,68 @@ class Patient < ApplicationRecord
 		event :start_responding do
 			transitions :from => :questionnaires, :to => :responding,
 									:after => Proc.new {|*args| ask_question(*args)},
-									:guard => Proc.new {|*args| questionnaire_finished?(*args)}
+									:guard => Proc.new {|*args| questionnaire_is_not_finished?(*args)}
 			transitions :from => :questionnaires, :to => :questionnaires,
 									:after => Proc.new {|*args| inform_wrong_questionnaire(*args)}
+		end
+
+		event :respond do
+			transitions :from => :responding, :to => :idle,
+									:after => Proc.new {|*args| register_last_response(*args)},
+									:guard => Proc.new {|*args| is_last_question_and_is_response?(*args)}
+			transitions :from => :responding, :to => :responding,
+									:after => Proc.new {|*args| register_response(*args)},
+									:guard => Proc.new {|*args| is_response?(*args)}
+			transitions :from => :responding, :to => :responding,
+									:after => :ask_last_question_again
 		end
 
 		event :cancel do
 			transitions :from => :questionnaires, :to => :idle, :after => :back_to_menu
 			transitions :from => :responding, :to => :idle, :after => :back_to_menu
 		end
+
+		event :no_action do
+			transitions :from => :idle, :to => :idle,
+									:after => :send_no_action_received
+		end
+
 	end
 
 	private
+
+	def register_last_response(response)
+		Manager.new(self).register_response(response)
+		Dialogue.new(self).send_questionnaire_finished
+	end
+
+	def is_last_question_and_is_response?(response)
+		if is_response?(response) && Manager.new(self).is_last_question?
+			ap 'TRUE'
+			true
+		else
+			ap 'FALSE'
+			false
+		end
+	end
+
+	def ask_next_question
+		bot_command_data = JSON.parse(Dialog.where('patient_id = ? AND bot_command_data IS NOT NULL', self.id).last.bot_command_data)
+		Manager.new(self).ask_question(Questionnaire.find(bot_command_data['responding']['questionnaire_id']).title)
+	end
+
+	def ask_last_question_again
+		Manager.new(self).ask_last_question_again
+	end
+
+	def register_response(response)
+		Manager.new(self).register_response(response)
+		ask_next_question
+	end
+
+	def is_response?(response)
+		Manager.new(self).is_response?(response)
+	end
 
 	def inform_wrong_questionnaire(text)
 		Dialogue.new(self).inform_wrong_questionnaire(text)
@@ -45,8 +95,8 @@ class Patient < ApplicationRecord
 		Manager.new(self).ask_question(questionnaire)
 	end
 
-	def questionnaire_finished?(questionnaire)
-		Manager.new(self).questionnaire_finished?(questionnaire)
+	def questionnaire_is_not_finished?(questionnaire)
+		Manager.new(self).questionnaire_is_not_finished?(questionnaire)
 	end
 
 	def show_questionnaires
@@ -57,11 +107,19 @@ class Patient < ApplicationRecord
 		Manager.new(self).has_questionnaires?
 	end
 
+	def send_no_action_received
+		Dialogue.new(self).inform_no_action_received
+	end
+
 	def back_to_menu
 		Dialogue.new(self).back_to_menu_with_menu
 	end
 
 	def inform_no_questionnaires
 		Dialogue.new(self).inform_no_questionnaires
+	end
+
+	def no_action_received
+		Dialogue.new(self).inform_no_action_received
 	end
 end
