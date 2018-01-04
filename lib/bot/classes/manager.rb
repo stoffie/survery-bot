cadfdaef49fb3920e06d7f0080a33d5da0612629
neboduus <neboduus@gmail.com>
2018-01-04 15:52:17 +0100

    finalizes questionnaires management v_1

--- lib/bot/classes/manager.rb
+++ lib/bot/classes/manager.rb
@@ -7,16 +7,56 @@ class Manager
     @patient = patient
   end
 
-  def ask_question(q_name)
-    invitations = Invitation.where(patient_id: @patient.id)
-    invitations.each do |invitation|
-      questionnaire = Questionnaire.where(id: invitation.campaign.questionnaire_id, title: q_name).first
-      question = questionnaire.questions[invitation.answers.count]
-      Dialogue.new(@patient).ask_question(question, invitation)
+  def is_last_question?
+    bot_command_data = JSON.parse(Dialog.where('patient_id = ? AND bot_command_data IS NOT NULL', @patient.id).last.bot_command_data)
+    questionnaire = Questionnaire.find(bot_command_data['responding']['questionnaire_id'])
+    invitation = Invitation.find(bot_command_data['responding']['invitation_id'])
+    if questionnaire.questions.count-1 == invitation.answers.count
+      true
+    else
+      false
     end
   end
 
-  def questionnaire_finished?(q_name)
+  def ask_last_question_again
+    bot_command_data = JSON.parse(Dialog.where('patient_id = ? AND bot_command_data IS NOT NULL', @patient.id).last.bot_command_data)
+    question = Question.find(bot_command_data['responding']['question_id'])
+    invitation = Invitation.find(bot_command_data['responding']['invitation_id'])
+    dialog_manager = Dialogue.new(@patient)
+    dialog_manager.inform_wrong_response
+    dialog_manager.ask_question(question, invitation)
+  end
+
+  def register_response(response)
+    bot_command_data = JSON.parse(Dialog.where('patient_id = ? AND bot_command_data IS NOT NULL', @patient.id).last.bot_command_data)
+    Answer.create(invitation_id: bot_command_data['responding']['invitation_id'],
+                  question_id: bot_command_data['responding']['question_id'], text: response)
+    Dialogue.new(@patient).send_response_saved
+  end
+
+  def is_response?(response)
+    bot_command_data = JSON.parse(Dialog.where('patient_id = ? AND bot_command_data IS NOT NULL', @patient.id).last.bot_command_data)
+    question = Question.find(bot_command_data['responding']['question_id'])
+    options = question.options.map(&:text)
+    options.include?(response) ? true : false
+  end
+
+  def ask_question(q_name)
+    ap "IN ask_question(q_name='#{q_name}')"
+
+    invitations = Invitation.where(patient_id: @patient.id)
+    invitations.each do |invitation|
+      questionnaire = Questionnaire.where(id: invitation.campaign.questionnaire_id, title: q_name).first
+      ap "QUESTIONNAIRE="
+      ap questionnaire
+      unless questionnaire.nil?
+        question = questionnaire.questions[invitation.answers.count]
+        Dialogue.new(@patient).ask_question(question, invitation)
+      end
+    end
+  end
+
+  def questionnaire_is_not_finished?(q_name)
     invitations = Invitation.where(patient_id: @patient.id)
     invitations.each do |invitation|
       questionnaire = Questionnaire.where(id: invitation.campaign.questionnaire_id, title: q_name).first
